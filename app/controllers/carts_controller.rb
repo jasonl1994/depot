16aa8bfcb105e929623e675195f1f51bb45844e6
Jason Lai <jokerlai1994@gmail.com>
2017-03-01 21:50:54 +0000

    enhaced shopping cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,6 +1,7 @@
 class CartsController < ApplicationController
   before_action :set_cart, only: [:show, :edit, :update, :destroy]
-
+  rescue_from ActiveRecord::RecordNotFound, with: :invalid_cart
+  
   # GET /carts
   # GET /carts.json
   def index
@@ -53,13 +54,15 @@ class CartsController < ApplicationController
 
   # DELETE /carts/1
   # DELETE /carts/1.json
-  def destroy
-    @cart.destroy
+   def destroy
+    @cart.destroy if @cart.id == session[:cart_id]
+    session[:cart_id] = nil
     respond_to do |format|
-      format.html { redirect_to carts_url, notice: 'Cart was successfully destroyed.' }
+      format.html { redirect_to root_url,
+        notice: 'Your cart is currently empty' }
       format.json { head :no_content }
     end
-  end
+   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
@@ -70,5 +73,9 @@ class CartsController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def cart_params
       params.fetch(:cart, {})
+    end
+    def invalid_cart
+      logger.error "Attempt to access invalid cart #{params[:id]}"
+      redirect_to root_url, notice: 'Invalid cart'
     end
 end
